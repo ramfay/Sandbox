70217538c2ad2bdd7338397c9baaf4c760e44b84
Ramisa <candy_random@live.com.au>
2016-08-22 19:43:59 +1000

    Add name printing program

--- odd_name.py
+++ odd_name.py
@@ -1,3 +1,11 @@
 """
 Ramisa
 """
+
+name = str(input("Please enter your name"
+                 "\n> "))
+while name == "":
+    print("Invalid input!")
+    name = str(input("Please enter your name"
+                     "\n> "))
+print(name[::2])
